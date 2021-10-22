e9f7c85d162567fa4552e4b774d3126201bf3e95
Regena Sherly Padandana <62322763+Regenspadandanan@users.noreply.github.com>
2021-10-22 13:47:55 +0800

    initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+class Main {
+
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Regens", 22, 155);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+  
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+      hero.duduk();
+    }
+}
